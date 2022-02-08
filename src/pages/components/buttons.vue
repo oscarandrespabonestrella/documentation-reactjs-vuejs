627962f2d3649fa68d941fab62ba40c68840ca3d
PABON ESTRELLA Oscar Andres <oscarandres.pabonestrella@ext.europarl.europa.eu>
2022-02-08 06:46:15 +0100

    first commit

---
+++ src/pages/components/buttons.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import CodeBox from '../../components/CodeBox.vue'
+</script>
+
+<template>
+    <div class="grid grid-cols-2">
+        <CodeBox>
+            <h2>rtreas</h2>
+        </CodeBox>
+        <CodeBox>
+            <h2>rtreas</h2>
+        </CodeBox>
+    </div>
+</template>
